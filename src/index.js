421bfcfca8a817eb30d93c17eff1d7c96af228a5
Jose García Máñez <josegarciamanez@gmail.com>
2018-08-29 11:20:33 +0200

    boton cambio state

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 export default class Welcome extends React.Component {
+  state = { nombre: "Jose" };
+
+  handleClick = () => {
+    if (this.state.nombre === "Jose") {
+      this.setState({ nombre: "Juan" });
+    } else if (this.state.nombre === "Juan") {
+      this.setState({ nombre: "Jose" });
+    }
+  };
+
   render() {
     return (
       <div className="Bienvenida">
         <h1>Hola React</h1>
         <h2>Ejercicios con React</h2>
+        <h3 style={{ color: "green" }}>{this.state.nombre}</h3>
+        <button onClick={this.handleClick}>Cambiar Nombre</button>
       </div>
     );
   }
