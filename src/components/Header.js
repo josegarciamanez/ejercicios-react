9b9ef2e25e9fceeb82af74fe09169b5a8def9452
Jose García Máñez <josegarciamanez@gmail.com>
2018-10-25 15:52:33 +0200

    cambios header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 
 class Header extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          background: "white",
-          color: "black",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "fixed",
-          width: "100%",
-          top: "0"
-        }}
-      >
-        <p>Header Component</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div
+                style={{
+                    background: "white",
+                    color: "black",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    position: "fixed",
+                    width: "100%",
+                    top: "0",
+                    fontFamily: "sans-serif"
+                }}
+            >
+                <p>{this.props.titulo.toUpperCase()}</p>
+            </div>
+        );
+    }
 }
 
 export default Header;
--- src/pages/App.js
+++ src/pages/App.js
@@ -2,29 +2,35 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 
 class App extends Component {
-  state = { nombre: "Jose" };
+    state = {
+        nombre: "Jose",
+        apellidos: "García"
+    };
 
-  handleClick = () => {
-    if (this.state.nombre === "Jose") {
-      this.setState({ nombre: "Juan" });
-    } else if (this.state.nombre === "Juan") {
-      this.setState({ nombre: "Jose" });
+    handleClick = () => {
+        if (Object.is(this.state.nombre, "Jose")) {
+            this.setState({ nombre: "Juan" });
+        } else if (Object.is(this.state.nombre, "Juan")) {
+            this.setState({ nombre: "Jose" });
+        }
+        console.log(this.state);
+    };
+
+    render() {
+        return (
+            <div className="main-container">
+                <Header titulo="header con props" />
+                <div className="Bienvenida">
+                    <h1>Hola React</h1>
+                    <h2>Ejercicios con React</h2>
+                    <h3 style={{ color: "green" }}>{`${this.state.nombre} ${
+                        this.state.apellidos
+                    }`}</h3>
+                    <button onClick={this.handleClick}>Cambiar Nombre</button>
+                </div>
+            </div>
+        );
     }
-  };
-
-  render() {
-    return (
-      <div className="main-container">
-        <Header />
-        <div className="Bienvenida">
-          <h1>Hola React</h1>
-          <h2>Ejercicios con React</h2>
-          <h3 style={{ color: "green" }}>{this.state.nombre}</h3>
-          <button onClick={this.handleClick}>Cambiar Nombre</button>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default App;
